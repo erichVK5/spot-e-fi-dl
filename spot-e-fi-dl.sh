132ab50ad735731ee0c67f260744f065410fd9bf
erichVK5 <a1039181@gmail.com>
2022-10-04 16:17:27 +1030

    Add comments to screenshot script

--- spot-e-fi-dl.sh
+++ spot-e-fi-dl.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# the for loop goes to 52, assuming 7 songs per screen, and about 350 songs in the playlist
+# the loop range will need to be modified depending on the playlist length, and the height of the screen
+
 sleep 2
 for i in {1..52}
 do
